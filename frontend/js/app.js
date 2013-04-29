0384a889e6efef7190e60f5b1b3715709afb954a
Prace Fen <fenprace@gmail.com>
2013-04-30 00:30:19 +0800

    Merge controller.js and app.js

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -1,8 +1,27 @@
-angular.module('textcast', []).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.
-      when('/posts', {templateUrl: 'frontend/partials/posts.html', controller: PostsCtrl}).
-      when('/post/:postUrl', {templateUrl: 'frontend/partials/post.html', controller: PostCtrl}).
-      otherwise({redirectTo: '/posts'});
-    }
-  ]);
+var TextCast = angular.module('TextCast', [])
+
+// PostsCtrl (index)
+TextCast.controller('PostsCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('data/metadata.json').success(function(data) {
+    $scope.posts = data;
+  });
+}]);
+
+
+// PostCtrl (show)
+TextCast.controller('PostCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+  var dataUrl = 'data/' + $routeParams.postUrl + '.json';
+  
+  $http.get(dataUrl).success(function(data) {
+    $scope.post = data;
+  });
+}]);
+
+// Routes
+TextCast.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.
+    when('/posts', {templateUrl: 'frontend/partials/posts.html', controller: 'PostsCtrl'}).
+    when('/post/:postUrl', {templateUrl: 'frontend/partials/post.html', controller: 'PostCtrl'}).
+    otherwise({redirectTo: '/posts'});
+  }
+]);
